4086d7f968ede46bab19285d83f1aedbcece1265
nyto9999 <49970020+nyto9999@users.noreply.github.com>
2022-02-06 16:10:35 +0800

    init

---
+++ PokemonAlamofile/PokemonAlamofile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonAlamofile
+//
+//  Created by 宇宣 Chen on 2022/2/6.
+//
+
+import UIKit
+import Alamofire
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
